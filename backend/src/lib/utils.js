83e6402eead0cd4f417ee1bfd1941d230665fb87
akshay-k-bhat-coder <akshaykbhat702@gmail.com>
2026-02-08 18:13:47 +0530

    signup section code refactoring

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,6 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not set");
+  }
+  const isProd = process.env.NODE_ENV === "production";
+
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
@@ -9,7 +14,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // in MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
     sameSite: "strict", //CSRF attacks
-    secure: process.env.NODE_ENV === "development" ? false : "true",
+    secure: isProd,
   });
 
   return token;
