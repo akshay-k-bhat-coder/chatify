3572b6d2ba95e5d839d67a7d25561aa99ed59be9
akshay-k-bhat-coder <akshaykbhat702@gmail.com>
2026-02-08 17:39:41 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,24 +3,30 @@ import dotenv from "dotenv";
 import authRoutes from "./Routes/auth.route.js";
 import messageRoutes from "./Routes/message.route.js";
 import path from "path";
+import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
 
-const __dirname=path.resolve();
+const __dirname = path.resolve();
 
-const PORT=process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use("/api/auth",authRoutes);
+app.use(express.json()); //req.body
 
-app.use("/api/messages",messageRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use("/api/messages", messageRoutes);
 
 //make ready for deployement
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-    app.get("*",(req,res) => {
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-app.listen(PORT, () => console.log("Server running on port: "+PORT));
+app.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+  connectDB();
+});
